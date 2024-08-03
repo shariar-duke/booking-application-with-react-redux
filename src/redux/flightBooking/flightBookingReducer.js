453d4307d7a04c88a3df9256e38f5cae8b83ecc2
shariar <shariarmahmud225@gmail.com>
2024-08-03 20:13:29 +0600

    changes in reducer function

--- src/redux/flightBooking/flightBookingReducer.js
+++ src/redux/flightBooking/flightBookingReducer.js
@@ -2,31 +2,36 @@
 /* eslint-disable no-unused-vars */
 import { ADDBOOKING, DELETEBOOKING } from "./actionsTypes";
 
-const initialState = [
-    {
-        id: 1,
-        destinationForm: "Dhaka",
-        destinationTo: "SaidPur",
-        journeyDate: new Date(2024, 6, 5),
-        numberOfGuests: 4,
-        flightClass: "Business"
-
-    }
-]
+const initialState = {
+    value: [
+        {
+            id: 1,
+            destinationForm: "Dhaka",
+            destinationTo: "SaidPur",
+            journeyDate: new Date(2024, 6, 5),
+            numberOfGuests: 4,
+            flightClass: "Business"
+        }
+    ]
+}
 
 const flightReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADDBOOKING:
-            return [
+            return {
                 ...state,
-                action.payload
-            ]
+                value: [
+                    ...state.value,
+                    action.payload
+                ]
+            };
         case DELETEBOOKING:
-            // eslint-disable-next-line no-case-declarations
-            const newList = state.filter((flight) => flight.id != action.payload)
-            return [
-                ...newList
-            ]
+            return {
+                ...state,
+                value: state.value.filter(flight => flight.id !== action.payload)
+            };
+        default:
+            return state;
     }
 
 
