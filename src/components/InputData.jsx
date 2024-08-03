14a16db672619cc5b6b1d732fbd17974b635c8c4
shariar <shariarmahmud225@gmail.com>
2024-08-03 21:01:01 +0600

    package installed and forms are ready to submit

--- src/components/InputData.jsx
+++ src/components/InputData.jsx
@@ -2,18 +2,46 @@ import framImg from "../assets/Frame.svg";
 import guestIcon from "../assets/guestIcon.svg";
 import flightClass from "../assets/flightClass.svg"
 import { FaPlus } from "react-icons/fa";
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid'; // Import UUID
 export default function InputData() {
+    const [formData, setFormData] = useState({
+        id: uuidv4(),
+        destinationForm: '',
+        destinationTo: '',
+        journeyDate: '',
+        numberOfGuests: '',
+        flightClass: '',
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
+        console.log("the formData is now is", formData)
+
+    }
+
+
+
     return (
         <div className="relative ">
             <div >
-                <form className="first-hero lws-inputform">
+                <form onSubmit={handleSubmit} className="first-hero lws-inputform">
                     <div className="bg-white  rounded-md flex gap-[20px] p-[20px] ">
                         {/* Destination From  */}
                         <div className="border-r border-2px border-gray-400 pr-[30px]" >
                             <p className="text-[18px] text-gray-400">Destination From</p>
                             <div className="mt-[8px] flex gap-[0px]  items-center ">
                                 <img src={framImg} className="h-[30px]" alt="" />
-                                <select className="outline-none px-2 py-2 w-full" name="from" id="lws-from" required>
+                                <select onChange={handleChange} className="outline-none px-2 py-2 w-full" name="destinationForm" id="lws-from" required>
                                     <option value="" hidden>Please Select</option>
                                     <option>Dhaka</option>
                                     <option>Sylhet</option>
@@ -27,7 +55,7 @@ export default function InputData() {
                             <p className="text-[18px] text-gray-400">Destination To</p>
                             <div className="mt-[8px] flex gap-[0px]  items-center">
                                 <img src={framImg} className="h-[30px]" alt="" />
-                                <select className="outline-none px-4 py-2 w-full" name="from" required>
+                                <select onChange={handleChange} className="outline-none px-4 py-2 w-full" name="destinationTo" required>
                                     <option value="" hidden>Please Select</option>
                                     <option>Dhaka</option>
                                     <option>Sylhet</option>
@@ -38,13 +66,13 @@ export default function InputData() {
                         </div>
                         <div className="border-r border-2px border-gray-400 pr-[30px]" >
                             <p className="text-[18px] text-gray-400">Journey Date</p>
-                            <input type="date" className="outline-none px-2 py-2 mt-[6px] w-full date" name="date" required />
+                            <input onChange={handleChange} type="date" className="outline-none px-2 py-2 mt-[6px] w-full date" name="journeyDate" required />
                         </div>
                         <div className="border-r border-2px border-gray-400 pr-[30px]" >
                             <p className="text-[18px] text-gray-400">Guests</p>
                             <div className="mt-[8px] flex gap-[0px]  items-center">
                                 <img src={guestIcon} className="h-[26px]" alt="" />
-                                <select className="outline-none px-2 py-2 w-full" name="from" required>
+                                <select onChange={handleChange} className="outline-none px-2 py-2 w-full" name="numberOfGuests" required>
                                     <option value="" hidden>Please Select</option>
                                     <option value="1">1 Person</option>
                                     <option value="2">2 Persons</option>
@@ -57,7 +85,7 @@ export default function InputData() {
                             <p className="text-[18px] text-gray-400">Class</p>
                             <div className="mt-[8px] flex gap-[0px]  items-center">
                                 <img src={flightClass} className="h-[26px]" alt="" />
-                                <select className="outline-none px-2 py-2 w-full" name="from" required>
+                                <select onChange={handleChange} className="outline-none px-2 py-2 w-full" name="flightClass" required>
                                     <option value="" hidden>Please Select</option>
                                     <option>Business</option>
                                     <option>Economy</option>
@@ -66,7 +94,7 @@ export default function InputData() {
                         </div>
 
                         <div className=" flex justify-center items-center" >
-                            <button className="border border-purple-400 px-4 py-2 rounded-md text-purple-700 font-bold text-[18px] flex gap-[6px] items-center">
+                            <button type="submit" className="border border-purple-400 px-4 py-2 rounded-md text-purple-700 font-bold text-[18px] flex gap-[6px] items-center">
                                 <FaPlus fontSize={14} />
                                 <p>Book</p>
                             </button>
